f9b98c05c50fdc750f69d383e1894f7688ee1952
mankurtas <povis.vasia@gmail.com>
2022-10-01 22:01:44 +0300

    added functionality

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,28 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {}
+
+Calculator.prototype.sum = function (number1, number2) {
+  return number1 + number2;
+};
+
+Calculator.prototype.subtraction = function (number1, number2) {
+  return number1 - number2;
+};
+
+Calculator.prototype.multiplication = function (number1, number2) {
+  return number1 * number2;
+};
+
+Calculator.prototype.division = function (number1, number2) {
+  return number1 / number2;
+};
+
+const test1 = new Calculator();
+
+console.log(test1.sum(5, 5));
+console.log(test1.subtraction(5, 5));
+console.log(test1.multiplication(5, 5));
+console.log(test1.division(5, 5));
