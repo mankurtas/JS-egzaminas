1bbfe6641288b742eee3594e72558b748fe4493e
mankurtas <povis.vasia@gmail.com>
2022-10-01 17:45:49 +0300

    functionality added

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,39 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+const btnShowUsers = document.getElementById("btn");
+const outputDiv = document.getElementById("output");
+
+const showUsers = async () => {
+  try {
+    const resolve = await fetch(ENDPOINT);
+    const data = await resolve.json();
+    console.log(data);
+    createUserCard(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createUserCard = (data) => {
+  outputDiv.innerHTML = "";
+
+  data.forEach((element) => {
+    const divCard = document.createElement("div");
+    divCard.setAttribute("class", "card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", element.avatar_url);
+    img.setAttribute("alt", "Avatar");
+
+    const divContainer = document.createElement("div");
+    divContainer.setAttribute("class", "container");
+    divContainer.innerHTML = element.login;
+
+    divCard.append(img, divContainer);
+    outputDiv.append(divCard);
+  });
+};
+
+btnShowUsers.addEventListener("click", showUsers);
