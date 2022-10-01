5621847434a64c851bd594a04435752ff861d586
mankurtas <povis.vasia@gmail.com>
2022-10-01 21:22:33 +0300

    added funtionality

--- Task 6/script.js
+++ Task 6/script.js
@@ -9,13 +9,34 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+const getUserAverageAge = (users) => {
+  let sum = 0;
+  let length = users.length;
+
+  users.forEach((element) => {
+    sum += element.age;
+  });
+
+  return Math.round(sum / length);
+};
+
+console.log(getUserAverageAge(users));
+
+const getUsersNames = (users) => {
+  const namesArray = users.map((element) => element.name);
+
+  return namesArray;
+};
+
+console.log(getUsersNames(users));
