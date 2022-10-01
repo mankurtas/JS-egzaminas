5e0760f8a1c37c8f0fdb4c5e43b00eb72c0999a4
mankurtas <povis.vasia@gmail.com>
2022-10-01 12:22:53 +0300

    added functionality

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,38 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const input = document.getElementById("search");
+const submit = document.getElementById("submit-btn");
+const output = document.getElementById("output");
+
+const toLbs = (weight) => weight * 2.2046;
+const toGrams = (weight) => weight / 0.001;
+const toOz = (weight) => weight * 35.274;
+
+const convertWeight = (e) => {
+  e.preventDefault();
+  const inputWeight = input.value;
+
+  if (isNaN(inputWeight)) {
+    output.innerHTML = "You entered not a number. Please enter a number.";
+    input.value = "";
+  } else {
+    console.log(input.value);
+    output.innerHTML =
+      "Svoris svarais: " +
+      toLbs(inputWeight) +
+      " lb." +
+      "<br />" +
+      "Svoris gramais: " +
+      toGrams(inputWeight) +
+      " g." +
+      "<br />" +
+      "Svoris uncijomis: " +
+      toOz(inputWeight) +
+      " oz.";
+
+    input.value = "";
+  }
+};
+
+submit.addEventListener("click", convertWeight);
